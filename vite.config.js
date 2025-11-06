256678351157567542ae355a67e423ab7f4b732d
lidiya <karavaeva.lidaa@yandex.ru>
2025-11-06 15:14:56 +0700

    fix: correct vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/todo_vue/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // ← твой src
