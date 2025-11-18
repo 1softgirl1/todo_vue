7151d9c2e0e4033d790906a5ad0652a3a2216c35
lidiya <karavaeva.lidaa@yandex.ru>
2025-11-18 16:52:01 +0700

    add pinia

--- src/App.vue
+++ src/App.vue
@@ -45,48 +45,36 @@
 </template>
 
 <script setup>
-  import { ref, computed, watch, onMounted } from 'vue';
+  import { ref, computed } from 'vue';
   import AddTodoModal from './components/AddTodoModal.vue';
   import TodoList from './components/TodoList.vue';
   import SearchBar from './components/SearchBar.vue';
   import ThemeToggle from "@/components/ThemeToggle.vue";
 
+  import { useDataStore } from '@/stores/useDataStore.js'
 
-  const tasks = ref([]);
+  const store = useDataStore()
+
   const isModalOpen = ref(false);
   const editingTask = ref(null);
   const searchQuery = ref('');
 
-  onMounted(() => {
-    try {
-      const saved = JSON.parse(localStorage.getItem('tasks') || '[]');
-      tasks.value = Array.isArray(saved) ? saved : [];
-    } catch {
-      tasks.value = [];
-    }
-  });
-
-  watch(
-      tasks,
-      (newVal) => {
-        localStorage.setItem('tasks', JSON.stringify(newVal));
-      },
-      { deep: true }
-  );
+  const tasks = computed(() => store.tasks)
 
   const todoCount = computed(() => tasks.value.filter(t => !t.done).length);
   const doneCount = computed(() => tasks.value.filter(t => t.done).length);
 
   const filteredTasks = computed(() =>
       tasks.value.filter(
-          (t) =>
+          t =>
               !t.done &&
               t.text.toLowerCase().includes(searchQuery.value.trim().toLowerCase())
       )
   );
+
   const doneList = computed(() =>
       tasks.value.filter(
-          (t) =>
+          t =>
               t.done &&
               t.text.toLowerCase().includes(searchQuery.value.trim().toLowerCase())
       )
@@ -101,31 +89,33 @@
     isModalOpen.value = false;
     editingTask.value = null;
   }
+
   function handleAdd(task) {
-    tasks.value = [task, ...tasks.value];
+    store.addTask(task)
   }
+
   function handleUpdate({ id, text }) {
-    tasks.value = tasks.value.map(t => (t.id === id ? { ...t, text } : t));
+    store.updateTask({ id, text })
   }
+
   function deleteTask(id) {
-    tasks.value = tasks.value.filter((t) => t.id !== id);
+    store.deleteTask(id)
   }
 
   function toggleTask(id) {
-    tasks.value = tasks.value.map((t) =>
-        t.id === id ? { ...t, done: !t.done } : t
-    );
-  }
-  function startEdit(id) {
-    const found = tasks.value.find((t) => t.id === id);
-    if (found) {
-      editingTask.value = { ...found };
-      isModalOpen.value = true;
-    }
-  }
-  function onSearch(query) {
-    searchQuery.value = query;
+    store.toggleTask(id)
   }
 
+  function startEdit(id) {
+    const found = tasks.value.find(t => t.id === id)
+    if (found) {
+      editingTask.value = { ...found }
+      isModalOpen.value = true
+    }
+  }
+
+  function onSearch(query) {
+    searchQuery.value = query
+  }
 </script>
 
